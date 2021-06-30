9b32be7cef33e4045c2ee62e0af4d00ab393a2b8
Ravi Assis <ravi.assis@hotmart.com>
2021-06-30 14:13:02 -0300

    Send register's count on get cashflow

--- data/index.js
+++ data/index.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 const pg = require('pg');
 const PG_DECIMAL_OID = 1700;
 pg.types.setTypeParser(PG_DECIMAL_OID, parseFloat);
+pg.types.setTypeParser(20, parseInt);
 const db = require('knex')(require('../knexfile')[env]);
 
 module.exports = db;
--- routes/cashflow.js
+++ routes/cashflow.js
@@ -4,6 +4,27 @@ const db = require('../data');
 const constants = require('../util/constants');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 
+class CashFlowTypeEnum {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static INCOMING = new CashFlowTypeEnum(0, 'Entrada');
+    static OUTGOING = new CashFlowTypeEnum(1, 'Saída');
+
+    static getById(id) {
+        if(id === CashFlowTypeEnum.INCOMING.id) {
+            return CashFlowTypeEnum.INCOMING;
+        }
+        switch(id) {
+            case CashFlowTypeEnum.INCOMING:
+                return CashFlowTypeEnum.INCOMING;
+            case CashFlowTypeEnum.OUTGOING:
+                return CashFlowTypeEnum.OUTGOING;
+        }
+    }
+}
+
 router.get('/',
     validationMiddleware([
         query('limit').toInt(),
@@ -13,13 +34,16 @@ router.get('/',
         let { limit, offset } = req.query;
         limit = limit || constants.LIMIT;
         offset = offset || constants.OFFSET;
-        const cashflow = await db('cashflow')
+        let total = (await db('cashflow').count('*'))[0].count;
+        let cashflow = await db('cashflow')
                                 .orderBy([{column: 'datetime', order: 'desc'}])
                                 .limit(limit)
                                 .offset(offset);
+        cashflow.forEach(c => c.cashFlowType = CashFlowTypeEnum.getById(c.cashFlowType));
         res.status(200).json({
             limit,
             offset,
+            total,
             data: cashflow
         });  
     }
@@ -27,7 +51,10 @@ router.get('/',
 
 const createCashFlowValidations = [
     body('description').notEmpty().trim().escape().withMessage('Not be empty'),
-    body('cashFlowType').isIn([0, 1]).toInt().withMessage(`should be in [${[0,1].toString()}]`),
+    body('cashFlowType')
+        .isIn([CashFlowTypeEnum.INCOMING.id, CashFlowTypeEnum.OUTGOING.id])
+        .toInt()
+        .withMessage(`should be in [${[CashFlowTypeEnum.INCOMING.id, CashFlowTypeEnum.OUTGOING.id].toString()}]`),
     body('value').isFloat({min: 0}).toFloat().withMessage('should be a number greater than 0.0'),
     body('datetime').isISO8601().toDate().withMessage('should be a datetime in ISO 8601 UTC format')
 ];
